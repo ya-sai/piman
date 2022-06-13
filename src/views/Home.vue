1c0676d3fb6629331141b9aa9080970160e27f6b
Neil-Lin <neillin@blueplanet.com.tw>
2022-06-13 15:41:22 +0800

    fix: homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <section>
+      <h2>Home</h2>
+      <article>
+        <a href="https://github.com/ya-sai/piman" target="_blank" rel="noreferrer noopener">Github</a>
+        <br><br>
+        <a href="https://piman.notion.site/piman/Piman-6389e4787ceb405abe42ffb37cfe1ca4" target="_blank" rel="noreferrer noopener">Docs</a>
+      </article>
+    </section>
   </div>
 </template>
 
