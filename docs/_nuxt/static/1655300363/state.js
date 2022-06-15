8b97fd1acc82f48b4992052dc23fbd7410dd03a0
Neil Lin <neillin@blueplanet.com.tw>
2022-06-15 21:40:49 +0800

    fix: landing page

---
+++ docs/_nuxt/static/1655300363/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655300363",layout:"landingpage",error:a,serverRendered:true,routePath:b,config:{baseURL:"http:\u002F\u002Flocalhost:3000",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null,"\u002F"));
