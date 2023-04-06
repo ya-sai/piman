48db9c89504a69b9af3adcd50ebadca555d717fd
717chi <chi@blueplanet.com.tw>
2023-04-06 16:36:23 +0800

    feat: vue3 export components file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,17 @@
 import PiButton from "./Button/PiButton.vue";
+import PiCheckbox from "./Checkbox/PiCheckbox.vue";
+import PiCard from "./Card/PiCard.vue";
+import PiAccordion from "./Accordion/PiAccordion.vue";
+import PiAccordionItem from "./Accordion/PiAccordionItem.vue";
+import PiTabPanel from "./Tabs/PiTabPanel.vue";
+import PiTabs from "./Tabs/PiTabs.vue";
 
-export { PiButton }
+export {
+  PiButton,
+  PiCheckbox,
+  PiCard,
+  PiAccordion,
+  PiAccordionItem,
+  PiTabPanel,
+  PiTabs
+}
