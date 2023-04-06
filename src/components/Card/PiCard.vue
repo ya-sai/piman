69355161ef144c2bae8f57576ae2426fdf6b16b6
717chi <chi@blueplanet.com.tw>
2023-04-06 16:16:46 +0800

    feat: vue3 card

---
+++ src/components/Card/PiCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    :class="[
+      'pi-card',
+      shadow ? 'pi-card--shadow' : ''
+    ]"
+  >
+    <div
+      v-if="$slots.header || header"
+      class="pi-card-header"
+    >
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div
+      :class="[
+        'pi-card-container',
+        nopadding ? 'no-padding' : ''
+      ]"
+    >
+      <slot></slot>
+    </div>
+    <div
+      v-if="$slots.footer || footer"
+      class="pi-card-footer"
+    >
+      <slot name="footer">{{ footer }}</slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+defineProps({
+  header: {},
+  footer: {},
+  nopadding: Boolean,
+  shadow: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/DemoCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <article>
+    <section>
+      <h3>基本</h3>
+      <pi-card>
+        Cotnent
+      </pi-card>
+    </section>
+    <section>
+      <h3>內容無padding</h3>
+      <pi-card nopadding>
+        Cotnent
+      </pi-card>
+    </section>
+    <section>
+      <h3>客製化header</h3>
+      <pi-card header="卡片標題">
+        Cotnent
+      </pi-card>
+      <pi-card>
+        <template v-slot:header>
+          <mark>卡片標題</mark>
+          <pi-button theme="primary" size="small">查看更多</pi-button>
+        </template>
+        Cotnent
+      </pi-card>
+    </section>
+    <section>
+      <h3>客製化header+陰影</h3>
+      <pi-card shadow>
+        <template v-slot:header>
+          <mark>卡片標題</mark>
+          <pi-button theme="primary" size="small">查看更多</pi-button>
+        </template>
+        Cotnent
+        <template v-slot:footer>
+          <pi-button theme="primary">Button</pi-button>
+          <pi-button>Button</pi-button>
+        </template>
+      </pi-card>
+    </section>
+    <section>
+      <h3>客製化header+陰影+footer</h3>
+      <pi-card shadow>
+        <template v-slot:header>
+          <mark>卡片標題</mark>
+          <pi-button theme="primary" size="small">查看更多</pi-button>
+        </template>
+        Cotnent
+        <template v-slot:footer>
+          <pi-button theme="primary">Button</pi-button>
+          <pi-button>Button</pi-button>
+        </template>
+      </pi-card>
+    </section>
+    <section>
+      <h3>客製化header+陰影+footer+客製化內容</h3>
+      <pi-card shadow  nopadding>
+        <template v-slot:header>
+          <mark>卡片標題</mark>
+          <pi-button theme="primary" size="small">查看更多</pi-button>
+        </template>
+        <pi-accordion>
+          <pi-accordion-item>
+            內容1
+          </pi-accordion-item>
+          <pi-accordion-item>
+            內容2
+          </pi-accordion-item>
+        </pi-accordion>
+        <template v-slot:footer>
+          <pi-button theme="primary">Button</pi-button>
+          <pi-button>Button</pi-button>
+        </template>
+      </pi-card>
+    </section>
+  </article>
+</template>
+
+<script lang="ts" setup>
+//@ts-ignore
+import { PiButton, PiCard, PiAccordion, PiAccordionItem } from "/dist/piman-vue3.es";
+
+</script>
+
+<style lang="scss" scoped>
+</style>
